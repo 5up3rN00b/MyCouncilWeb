4c22d693ca3ce180518dc1d9febe2d06904fb531
5up3rN00b <aaron.linear@gmail.com>
2020-06-15 13:29:17 -0700

    Added get.php and add.php

---
+++ html/users/add.php
@@ -0,0 +1,29 @@
+<?php
+require '../../templates/helper.php';
+
+$db = setupDb();
+if (!$db) {
+    die('Could not load database!');
+}
+
+if (hasValue($_POST['first_name'])) {
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $zipcode = $_POST['zipcode'];
+    $hashedPw = hash('sha256', $password);
+
+    if (hasValue($_POST['branch'])) {
+        $sth = $db->prepare("INSERT INTO `users` (`email`, `branch`, `first_name`, `last_name`, `password`, `zipcode`) VALUES (?, ?, ?, ?, ?)");
+        $sth->execute([$email, $_POST['branch'], $first_name, $last_name, $hashedPw, $zipcode]);
+    } else {
+        $sth = $db->prepare("INSERT INTO `users` (`email`, `first_name`, `last_name`, `password`, `zipcode`) VALUES (?, ?, ?, ?)");
+        $sth->execute([$email, $first_name, $last_name, $hashedPw, $zipcode]);
+    }
+    if (!$sth) {
+        echo 'Register failed';
+    } else {
+        echo 'Registered successfully';
+    }
+}
---
+++ html/users/get.php
@@ -0,0 +1,25 @@
+<?php
+if (hasValue($_POST['email']) && hasValue($_POST['password'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sth = $db->prepare("SELECT * FROM `users` WHERE `email`=?");
+    $sth->execute([$email]);
+    $passArr = $sth->fetchAll();
+
+    if (hash('sha256', $password) == $passArr[0]['password']) {
+        if ($passArr[0]['branch'] == 'None') {
+            echo 'Citizen' . ':' . $passArr[0]['user_id'] . ':' . $email . ':' . $passArr[0]['first_name'] . ':' . $passArr[0]['last_name'] . ':' . $passArr[0]['zipcode'];
+        } else {
+            echo 'Leader' . ':' . $passArr[0]['user_id'] . ':' . $email . ':' . $passArr[0]['branch'] . ':' . $passArr[0]['first_name'] . ':' . $passArr[0]['last_name'] . ':' . $passArr[0]['zipcode'];
+        }
+    } else {
+        if (empty($passArr)) {
+            echo 'Email does not exist';
+        } else {
+            echo 'Login failed';
+        }
+    }
+} else {
+
+}
