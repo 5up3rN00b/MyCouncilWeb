7f650f1c8bb14817fd3438e61e92cb34f6b411a4
5up3rN00b <aaron.linear@gmail.com>
2020-06-16 21:39:58 -0700

    Added post get all

---
+++ html/posts/get.php
@@ -0,0 +1,17 @@
+<?php
+require '../../templates/helper.php';
+
+$db = setupDb();
+if (!$db) {
+    die('Could not load database!');
+}
+
+if (hasValue($_GET['all'])) {
+    $sth = $db->prepare("SELECT * FROM posts");
+    $sth->execute();
+    $passArr = $sth->fetchAll();
+
+    foreach ($passArr as $value) {
+        echo $value['post_id'] . '|' . $value['user_id'] . '|' . $value['title'] . '|' . $value['description'] . '|' . $value['upvotes'] . '|' . $value['downvotes'] . '<br>';
+    }
+}
